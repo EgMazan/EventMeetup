ffceab189b085fea51d273aa1c9d0121691966bd
EgMazan <1egor1mazan@gmail.com>
2025-12-31 13:39:00 +0300

    Create New Project
    (cherry picked from commit 59d1d30c508f352d7f9c06d52c2dbbf8d0127c32)

---
+++ src/test/java/com/example/EventMeetup/EventMeetupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventMeetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMeetupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
