0b565c5e67868b40711faccf3fe96015e07ed08f
EgMazan <1egor1mazan@gmail.com>
2026-01-03 19:28:02 +0300

    MP-3 Initial project settings

--- src/main/java/com/egorsproject/EventMeetup/EventMeetupApplication.java
+++ src/main/java/com/egorsproject/EventMeetup/EventMeetupApplication.java
@@ -1,4 +1,4 @@
-package com.example.EventMeetup;
+package com.egorsproject.EventMeetup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
