d467939cec4bf2b98d6b1b88545d87bb80c58282
EgMazan <1egor1mazan@gmail.com>
2026-01-10 14:07:40 +0300

    MP-4 corrections 1

--- src/main/java/com/egorsproject/eventmeetup/controller/UserController.java
+++ src/main/java/com/egorsproject/eventmeetup/controller/UserController.java
@@ -7,14 +7,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
     private final UserRepository repository;
+
     public UserController(UserRepository repository){
         this.repository=repository;
     }
+
     @GetMapping
     public List<User> getAll(){
         return repository.findAll();
--- src/main/java/com/egorsproject/eventmeetup/domain/User.java
+++ src/main/java/com/egorsproject/eventmeetup/domain/User.java
@@ -1,10 +1,8 @@
 package com.egorsproject.eventmeetup.domain;
 
 import jakarta.persistence.*;
-
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.Objects;
-
 
 @Entity
 @Table(name="users")
@@ -17,71 +15,57 @@ public class User {
     @Column(nullable = false)
     private String name;
     @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
+    private Instant createdAt;
     @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
+
     @PrePersist
-    protected void onCreate(){
-        createdAt=LocalDateTime.now();
-        updatedAt=LocalDateTime.now();
+    protected void onCreate() {
+        createdAt = Instant.now();
+        updatedAt = Instant.now();
     }
+
     @PreUpdate
-    protected void onUpdate(){
-        updatedAt=LocalDateTime.now();
+    protected void onUpdate() {
+        updatedAt = Instant.now();
     }
-    protected User(){}
+
+    protected User() {
+    }
 
     public long getId() {
         return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreatedAt() {
+    public Instant getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
+    public Instant getUpdatedAt() {
         return updatedAt;
     }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return id == user.id && email.equals(user.email) && name.equals(user.name);
+        if (!(o instanceof User user)) return false;
+        return getId() == user.getId() && getEmail().equals(user.getEmail()) && getName().equals(user.getName()) && getCreatedAt().equals(user.getCreatedAt()) && getUpdatedAt().equals(user.getUpdatedAt());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, email, name);
+        return Objects.hash(getId(), getEmail(), getName(), getCreatedAt(), getUpdatedAt());
     }
 }
+
+
+
+
