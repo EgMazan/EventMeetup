b692bf2e1c3f5ee01d50f9f48573c94afe1f8293
EgMazan <1egor1mazan@gmail.com>
2026-01-12 13:28:05 +0300

    MP-4 added log

--- src/main/java/com/egorsproject/eventmeetup/controller/UserController.java
+++ src/main/java/com/egorsproject/eventmeetup/controller/UserController.java
@@ -2,15 +2,20 @@ package com.egorsproject.eventmeetup.controller;
 
 import com.egorsproject.eventmeetup.dto.UserDto;
 import com.egorsproject.eventmeetup.service.UserService;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
     private final UserService userService;
 
     public UserController(UserService userService) {
@@ -19,6 +24,7 @@ public class UserController {
 
     @GetMapping
     public List<UserDto> getAll() {
+        log.info("Get /api/users was called");
         return userService.getAllUsers();
     }
 
